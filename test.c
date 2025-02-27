e3bcb3f6030004a4364109fc89e1766519f8fff2
dr-programmer <drtopuzov@gmail.com>
2025-02-27 21:36:30 +0200

    asd

--- test.c
+++ test.c
@@ -4,8 +4,8 @@
 
 int main() {
     printf("Hello, HackTUES11!\n");
-    int a = HACK_TUES_EDITION;
-    printf("HACK_TUES_EDITION = %d\n", a);
+    int a = MAGIC_NUM;
+    printf("MAGIC_NUM = %d\n", a);
     int b = a + 1;
     return 0;
 }
