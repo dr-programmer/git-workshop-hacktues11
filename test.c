cd6fc52bf198bcadeaf9089773df81d2a29fcd9c
dr-programmer <drtopuzov@gmail.com>
2025-02-27 21:05:02 +0200

    Added MAGIC_NUM

--- test.c
+++ test.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+#define MAGIC_NUM 6
+
 int main() {
     printf("Hello, HackTUES11!\n");
     printf("Hello, HackTUES11!!!!!!!!!!");
-    int a = 6;
-    printf("A = %d\n", a);
+    int a = MAGIC_NUM;
+    printf("MAGIC_NUM = %d\n", a);
     return 0;
 }
